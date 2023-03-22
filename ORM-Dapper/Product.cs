4b2b132836708f17308674d8f853b602ba51e573
Justin Huth <Jayhuth96@gmail.com>
2023-03-22 09:47:49 -0500

    Complete

---
+++ ORM-Dapper/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public int ProductID { get; set; }
+        public int Price { get; set; }
+        public int CategoryID { get; set; }
+        public bool OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,21 +1,44 @@
 ﻿using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
+using ORM_Dapper;
 using System.Data;
 
-namespace ORM_Dapper
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            var config = new ConfigurationBuilder()
-                 .SetBasePath(Directory.GetCurrentDirectory())
+
+ var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
                  .AddJsonFile("appsettings.json")
                  .Build();
 
-            string connString = config.GetConnectionString("DefaultConnection");
+string connString = config.GetConnectionString("DefaultConnection");
 
-            IDbConnection conn = new MySqlConnection(connString);
-        }
-    }
-}
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+departmentRepo.InsertDepartment("Johns new department");
+
+            var departments = departmentRepo.GetAllDepartments();
+
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department.DepartmentID);
+                Console.WriteLine(department.Name);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+                var productRepository = new DapperProductRepository(conn);
+var products = productRepository.GetAllProducts();
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.ProductID);
+                    Console.WriteLine(product.Name);
+                    Console.WriteLine(product.Price);
+                    Console.WriteLine(product.CategoryID);
+                    Console.WriteLine(product.OnSale);
+                    Console.WriteLine(product.StockLevel);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+
+
